ff4fe92160fc54f19a2110782be6a071d90f8a24
John Doe <johndoe@email.com>
2024-11-18 22:45:31 +0000

    first commit

---
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useContext } from 'react'
+import { PlayerContext } from '../context/PlayerContext'
+import Navbar from './Navbar'
+import AlbumItem from './AlbumItem'
+import SongItem from './SongItem'
+// import { albumsData } from '../assets/assets'
+// import { songsData } from '../assets/assets'
+
+const DisplayHome = () => {
+
+  const { songsData,albumsData } = useContext(PlayerContext);
+
+  return (
+    <>
+     <Navbar />
+     <div className='mb-4'>
+      <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+      <div className='flex overflow-auto'>
+          {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image}/>))}
+      </div>
+     </div>
+     <div className='mb-4'>
+      <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
+      <div className='flex overflow-auto'>
+          {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image} />)).slice(0, 6)}
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default DisplayHome
