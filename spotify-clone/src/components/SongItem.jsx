82d625dc601010a71eb7e09dbdc3f63639874c7b
John Doe <johndoe@email.com>
2024-11-24 16:02:55 +0000

    commit

--- spotify-clone/src/components/SongItem.jsx
+++ spotify-clone/src/components/SongItem.jsx
@@ -9,8 +9,14 @@ const SongItem = ({ name, image, desc, id }) => {
       onClick={() => playWithId(id)}
       className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]"
     >
-      <img loading="lazy" width="300" height="300" 
-      className="rounded " src={image} alt="song" />
+      <img
+        decoding="async"
+        width="300"
+        height="300"
+        className="rounded "
+        src={image}
+        alt="song"
+      />
       <p className="font-bold mt-2 mb-1">{name}</p>
       <p className="text-slate-200 text-sm">{desc}</p>
     </div>
